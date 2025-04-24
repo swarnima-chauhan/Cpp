19610ddfdf6570d2047c56486281561118326c18
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-24 15:29:14 +0530

    Floyd's triangle

--- pattern.cpp
+++ pattern.cpp
@@ -3,17 +3,24 @@ using namespace std;
 
 int main(){
     int n;
-    cout<<"enter the nunber:";
+    cout<<"enter the number:";
     cin>>n;
     int count=1;
     for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-            cout<<count;
+        for(int j=0;j<i+1;j++){
+            cout<<count<<" ";
             count++;
         }
         cout<<endl;
     }
 
+    // for(int i=0;i<n;i++){
+    //     for(int j=i+1;j>0;j--){
+    //         cout<<count<<" ";
+    //         count++;
+    //     }
+    //     cout<<endl;
+    // }
 
     return 0;
 }
