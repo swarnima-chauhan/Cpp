09d2dd81837f1980eed40f8fa7fdf6781b058dcf
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-26 03:45:02 +0530

    linear search

---
+++ arrays1/linearSearch.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int linearSearch(int arr[],int size,int target){
+    for(int i=0;i<size;i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[]={4,2,7,8,1,2,5};
+    int target=8;
+    int size=7;
+
+    cout<<target<<" is present at index "<<linearSearch(arr,size,target);
+
+    return 0;
+}
