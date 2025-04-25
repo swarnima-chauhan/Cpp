861f80d9d7e5efc38129f3ed65069823195b4aa4
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-26 04:54:29 +0530

    unique values of array

---
+++ arrays1/uniquevalues.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void printUnique(int arr[],int size){
+    for(int i=0;i<size;i++){
+        int count=0;
+        for(int j=0;j<size;j++){
+            if(i==j){
+                continue;
+            }
+            if(arr[i]==arr[j]){
+                count++;
+            }
+        }
+        if(count==0){
+            cout<<arr[i]<<" ";
+        }
+    }
+    
+}
+
+int main(){
+    int arr[]={1,2,3,3,2,1,5};
+    int size=7;
+
+    printUnique(arr,size);
+    return 0;
+}
