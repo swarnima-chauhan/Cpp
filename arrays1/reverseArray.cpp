8c27ce7d131a9bb159ab98a09eb2d1d0da81aaa9
swarnima-chauhan <swarnimc130101@gmail.com>
2025-04-26 04:09:19 +0530

    reversing an array with two pointers approach

---
+++ arrays1/reverseArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void reverseArray(int arr[],int size){
+    int start=0;
+    int end=size-1;
+    while(start<end){
+        //swap logic
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        //swap(arr[start],arr[end]);
+
+        start++;
+        end--;
+    }
+}
+
+int main(){
+    int arr[]={4,2,7,8,1,2,5};
+    int size=7;
+
+    reverseArray(arr,size);
+
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<< " ";
+    }
+}
