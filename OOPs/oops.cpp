d829138388b78cb68e73022abc41f345de712365
swarnima-chauhan <swarnimc130101@gmail.com>
2025-06-26 14:51:47 +0530

    example of shallow copy

--- OOPs/oops.cpp
+++ OOPs/oops.cpp
@@ -6,24 +6,25 @@ class Student
 {
 public:
     string name;
-    double cgpa;
+    double *cgpaPtr;
 
     Student(string name, double cgpa)
     {
         this->name = name;
-        this->cgpa = cgpa;
+        cgpaPtr = new double;
+        *cgpaPtr = cgpa;
     }
 
     Student(Student &s)
     {
         this->name = s.name;
-        this->cgpa = s.cgpa;
+        this->cgpaPtr = s.cgpaPtr; // Shallow copy
     }
 
     void getInfo()
     {
         cout << "Name: " << name << endl;
-        cout << "CGPA: " << cgpa << endl;
+        cout << "CGPA: " << *cgpaPtr << endl;
     }
 };
 
@@ -31,7 +32,9 @@ int main()
 {
     Student s1("Rahul Kumar", 8.9);
     Student s2(s1);
-    s2.getInfo();
+    s1.getInfo();
+    *(s2.cgpaPtr) = 9.5;
+    s1.getInfo();
 
     return 0;
 }
