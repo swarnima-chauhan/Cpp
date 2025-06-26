241e38c5608c50110b5f5f334b1dfda263fca4a3
swarnima-chauhan <swarnimc130101@gmail.com>
2025-06-26 14:15:53 +0530

    shallow copy

--- OOPs/oops.cpp
+++ OOPs/oops.cpp
@@ -2,78 +2,36 @@
 #include <string>
 using namespace std;
 
-class Teacher
+class Student
 {
-private:
-    double salary;
-
 public:
-    // non-parameterized constructor
-    // Teacher()
-    // {
-    //     cout << "Hi i am constructor\n";
-    // }
-    // parameterized constructor
-    Teacher(string name, string dept, string subject, double salary)
+    string name;
+    double cgpa;
+
+    Student(string name, double cgpa)
     {
         this->name = name;
-        this->dept = dept;
-        this->subject = subject;
-        this->salary = salary;
+        this->cgpa = cgpa;
     }
 
-    // copy constructor
-    Teacher(Teacher &t) // pass by reference
+    Student(Student &s)
     {
-        cout << "Custom Copy constructor called\n";
-        this->name = t.name;
-        this->dept = t.dept;
-        this->subject = t.subject;
-        this->salary = t.salary;
-    }
-
-    // properties
-    string name;
-    string dept;
-    string subject;
-
-    // methods/member functions
-    void changeDept(string newDept)
-    {
-        dept = newDept;
-    }
-
-    // setter
-    //  function to set the value of private member variable salary
-    void setSalary(double s)
-    {
-        salary = s;
-    }
-
-    // getter function
-    // to access private member variable salary
-    double getSalary()
-    {
-        return salary;
+        this->name = s.name;
+        this->cgpa = s.cgpa;
     }
 
     void getInfo()
     {
         cout << "Name: " << name << endl;
-        cout << "Department: " << dept << endl;
-        cout << "Subject: " << subject << endl;
-        cout << "Salary: " << salary << endl;
+        cout << "CGPA: " << cgpa << endl;
     }
 };
 
 int main()
 {
-
-    Teacher t1("Swarnima", "Computer Science", "C++", 50000); // parameterized constructor called here
-    // t1.getInfo();
-    cout << "Teacher 2 Info:\n";
-    Teacher t2(t1); // default copy constructor called here
-    t2.getInfo();
+    Student s1("Rahul Kumar", 8.9);
+    Student s2(s1);
+    s2.getInfo();
 
     return 0;
 }
