89b02f34ff3af131adcd2a687075b18b2083b064
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-08 22:58:12 +0530

    starting with sigma

---
+++ Sigma/starting/simpleInterest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int p = 200;
+    int r = 2;
+    int t = 2;
+
+    int SI;
+    SI = (p * r * t) / 100;
+
+    cout << SI;
+    return 0;
+}
