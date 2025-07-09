8dfa0c9c9a1288ac79f5b9a56c7bea4fe3a128af
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-10 00:23:24 +0530

    little modified

--- pattern/4.cpp
+++ pattern/4.cpp
@@ -6,11 +6,9 @@ int main()
     int num = 5;
     for (int i = 0; i < num; i++)
     {
-        int val = 1;
         for (int j = 0; j <= i; j++)
         {
-            cout << val;
-            val++;
+            cout << j + 1;
         }
         cout << endl;
     }
