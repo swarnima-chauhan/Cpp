bf1d20e873c2424c5c7cf01b0add543db10337bf
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-14 01:12:55 +0530

    Ass2 done perfect

---
+++ pointers/Ass2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void multipleBy2(int &a, int &b, int &c)
+{
+    a *= 2;
+    b *= 2;
+    c *= 2;
+}
+
+int main()
+{
+    int x = 1, y = 2, z = 3;
+    multipleBy2(x, y, z);
+    cout << x << y << z << "\n"; // it will show 2 4 6
+    return 0;
+}
