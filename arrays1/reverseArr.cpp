47e9abd347ee6783009cbcb0391ab33a822dcfc8
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-14 20:01:46 +0530

    Reverse an array program done with a little help

---
+++ arrays1/reverseArr.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void reverseArr(int *arr, int n)
+{
+    int l = 0;
+    int r = n - 1;
+
+    while (r > l)
+    {
+        int t = arr[l];
+        arr[l] = arr[r];
+        arr[r] = t;
+        l++;
+        r--;
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 6, 7, 4, 5, 8};
+    int n = sizeof(arr) / sizeof(int);
+
+    reverseArr(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+
+    return 0;
+}
