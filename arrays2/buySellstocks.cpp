91c7614cbdb61a2b7663ae71ab841b563183c0e4
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-25 14:17:14 +0530

    Buy Sell Stocks in O(n^2) time complexity

---
+++ arrays2/buySellstocks.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int buySell(int *prices, int n)
+{
+    int maxProfit = 0;
+    int profit = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            profit = prices[i] - prices[j];
+            maxProfit = max(profit, maxProfit);
+        }
+    }
+    return maxProfit;
+}
+
+int main()
+{
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int n = sizeof(prices) / sizeof(int);
+    cout << buySell(prices, n);
+
+    return 0;
+}
