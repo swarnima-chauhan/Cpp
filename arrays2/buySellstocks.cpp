bf0176652eec2631998a8a80c0bcc95088b15643
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-29 02:25:05 +0530

    shraddha didi's maxProfit

--- arrays2/buySellstocks.cpp
+++ arrays2/buySellstocks.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int buySell(int *prices, int n);
+void maxProfit(int *prices, int n)
+{
+    int bestBuy[100000];
+    bestBuy[0] = INT32_MAX;
+    cout << bestBuy[0] << ", ";
+    for (int i = 1; i < n; i++)
+    {
+        bestBuy[i] = min(bestBuy[i - 1], prices[i - 1]);
+        cout << bestBuy[i] << ", ";
+    }
+    int maxProfit = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int currProfit = prices[i] - bestBuy[i];
+        maxProfit = max(maxProfit, currProfit);
+    }
+    cout << endl
+         << maxProfit;
+}
+
+int buySell(int *prices, int n)
+{
+    int maxProfit = 0;
+    int profit = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            profit = prices[i] - prices[j];
+            maxProfit = max(profit, maxProfit);
+        }
+    }
+    return maxProfit;
+}
 
 int buySell2(int *prices, int n)
 {
@@ -44,22 +77,9 @@ int main()
 {
     int prices[] = {7, 1, 5, 3, 6, 4};
     int n = sizeof(prices) / sizeof(int);
-    cout << buySell2(prices, n);
+    // cout << buySell2(prices, n);
+
+    maxProfit(prices, n);
 
     return 0;
-}
-
-int buySell(int *prices, int n)
-{
-    int maxProfit = 0;
-    int profit = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            profit = prices[i] - prices[j];
-            maxProfit = max(profit, maxProfit);
-        }
-    }
-    return maxProfit;
 }
