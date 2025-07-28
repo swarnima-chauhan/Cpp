1c48a68909e327387a6bb80b08cf775a25a62fff
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-29 01:54:54 +0530

    my second method buySell2

--- arrays2/buySellstocks.cpp
+++ arrays2/buySellstocks.cpp
@@ -5,6 +5,26 @@ int buySell(int *prices, int n);
 
 int buySell2(int *prices, int n)
 {
+    int maxProfit = 0;
+    int profit = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int min = INT32_MAX;
+        for (int j = 0; j < i; j++)
+        {
+            if (min > prices[j])
+            {
+                min = prices[j];
+            }
+        }
+        profit = prices[i] - min;
+        maxProfit = max(profit, maxProfit);
+    }
+    return maxProfit;
+}
+
+int buySell3(int *prices, int n)
+{
     int min = prices[0];
     int maxP = 0;
     int pro = 0;
