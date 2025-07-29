d8e39328de4ff41ecca91ebb19ab14943398c422
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-29 15:12:11 +0530

    Shraddha didi's code

--- arrays2/trappingRainWater.cpp
+++ arrays2/trappingRainWater.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
 
-void myRa(int *bars, int n)
+void trap(int *heights, int n)
+{
+    int leftMax[20000];
+    leftMax[0] = heights[0];
+    // cout << leftMax[0] << ", ";
+    for (int i = 1; i < n; i++)
+    {
+        leftMax[i] = max(leftMax[i - 1], heights[i - 1]);
+        // cout << leftMax[i] << ", ";
+    }
+
+    int rightMax[20000];
+    rightMax[n - 1] = heights[n - 1];
+    // cout << rightMax[n - 1] << ", ";
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rightMax[i] = max(rightMax[i + 1], heights[i + 1]);
+        // cout << rightMax[i] << ", ";
+    }
+
+    int waterTrapped = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int currWater = min(leftMax[i], rightMax[i]) - heights[i];
+        if (currWater > 0)
+        {
+            waterTrapped += currWater;
+        }
+    }
+    cout << "water trapped : " << waterTrapped;
+}
+
+void myRa(int *bars, int n) // my try that is wrong
 {
     int left = 0;
     int right = 0;
@@ -21,8 +53,12 @@ void myRa(int *bars, int n)
         {
             awt = 0;
         }
+        else if (i == n - 1 || i == 0)
+        {
+            awt = 0;
+        }
+        cout << awt << " ";
         water += awt;
-        cout << water << " ";
     }
     cout << endl;
     cout << water;
@@ -32,6 +68,6 @@ int main()
 {
     int bars[] = {4, 2, 0, 6, 3, 2, 5};
     int n = sizeof(bars) / sizeof(int);
-    myRa(bars, n);
+    trap(bars, n);
     return 0;
 }
