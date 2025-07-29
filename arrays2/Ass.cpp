c545f1f6ede8c12ed552655971a75ec665e6e064
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-30 01:02:57 +0530

    part 2 of ass is completed

--- arrays2/Ass.cpp
+++ arrays2/Ass.cpp
@@ -16,11 +16,28 @@ bool Ass1(int *arr, int n)
     return false;
 }
 
+int Ass2(int *arr, int n, int target)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main()
 {
-    int arr[] = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
-    int n = sizeof(arr) / sizeof(int);
-    cout << Ass1(arr, n);
+    // int arr[] = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+    // int n = sizeof(arr) / sizeof(int);
+    // cout << Ass1(arr, n);
+
+    int arr2[] = {4, 5, 6, 7, 0, 1, 2};
+    int n2 = sizeof(arr2) / sizeof(int);
+
+    cout << Ass2(arr2, n2, 3);
 
     return 0;
 }
