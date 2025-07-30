20a7d2dee5b918588dc14d32ace13f5feff3299d
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-31 00:08:42 +0530

    Shraddha didi's code

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void bubble(int *arr, int n)
+void bubble(int *arr, int n) // my code(wrong a little)
 {
     for (int i = 0; i < n; i++)
     {
@@ -17,11 +17,27 @@ void bubble(int *arr, int n)
     }
 }
 
+void Bsort(int *arr, int n) // shraddha didi's code
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int t = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+            }
+        }
+    }
+}
+
 int main()
 {
     int arr[] = {5, 4, 1, 3, 2};
     int n = sizeof(arr) / sizeof(int);
-    bubble(arr, n);
+    Bsort(arr, n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
