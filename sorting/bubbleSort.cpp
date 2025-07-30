afe2cc5d0f83ad58be6bfcb0b99271217a62e061
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-30 23:44:55 +0530

    bubble sortt in descending order, my code

---
+++ sorting/bubbleSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void bubble(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {5, 4, 1, 3, 2};
+    int n = sizeof(arr) / sizeof(int);
+    bubble(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
