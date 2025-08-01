ad2f1904373e6b504ee8e670195fc61cc52f868b
swarnima-chauhan <swarnimc130101@gmail.com>
2025-08-01 19:07:25 +0530

    selection sort done shraddha didi

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -7,7 +7,7 @@ void selectionSort(int *arr, int n)
     for (int i = 0; i < n - 1; i++)
     {
         min = i;
-        for (int j = i; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
             if (arr[j] < arr[min])
             {
