0c2f3089fbb749b8c0c744719fdeb9c89979dccd
swarnima-chauhan <swarnimc130101@gmail.com>
2025-08-04 00:15:50 +0530

    starting counting sort

--- sorting/countsort.cpp
+++ sorting/countsort.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void countSort(int arr[], int n)
+{
+    int freq[100000]; // range
+    int minVal = INT32_MAX;
+    int maxVal = INT32_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    for (int i = minVal; i <= maxVal; i++)
+    {
+    }
+}
+
 int main()
 {
     return 0;
