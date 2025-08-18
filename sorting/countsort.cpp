5e7745a201680ffc70721af2b5b310467546a8b0
swarnima-chauhan <swarnimc130101@gmail.com>
2025-08-18 23:55:30 +0530

    count sort completed

--- sorting/countsort.cpp
+++ sorting/countsort.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 // counting sort,used when the range is short
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
 void countSort(int arr[], int n)
 {
     int freq[100000]; // range
@@ -12,12 +20,28 @@ void countSort(int arr[], int n)
         maxVal = max(maxVal, arr[i]);
     }
 
-    for (int i = minVal; i <= maxVal; i++)
+    // 1st step
+    for (int i = 0; i < n; i++)
     {
+        freq[arr[i]]++;
     }
+
+    // 2nd step
+    for (int i = minVal, j = 0; i <= maxVal; i++)
+    {
+        while (freq[i] > 0)
+        {
+            arr[j++] = i;
+            freq[i]--;
+        }
+    }
+
+    print(arr, n);
 }
 
 int main()
 {
+    int arr[8] = {1, 4, 1, 3, 2, 4, 3, 7};
+    countSort(arr, 8);
     return 0;
 }
