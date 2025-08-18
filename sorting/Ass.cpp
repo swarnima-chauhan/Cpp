284d10c682376e58ee52cef53b46a896c68aff37
swarnima-chauhan <swarnimc130101@gmail.com>
2025-08-19 01:31:53 +0530

    insertionSort

--- sorting/Ass.cpp
+++ sorting/Ass.cpp
@@ -48,8 +48,25 @@ void selectionSort(int arr[], int n)
     print(arr, n);
 }
 
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int curr = arr[i];
+        int prev = i - 1;
+        while (prev >= 0 && arr[prev] < curr)
+        {
+            swap(arr[prev], arr[prev + 1]);
+            prev--;
+        }
+        arr[prev + 1] = curr;
+    }
+    print(arr, n);
+}
+
 int main()
 {
     int arr[10] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
-    selectionSort(arr, 10);
+    insertionSort(arr, 10);
+    return 0;
 }
