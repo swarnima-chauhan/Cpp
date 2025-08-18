382005af914429a9b94cd6a322c95d854fc5270d
swarnima-chauhan <swarnimc130101@gmail.com>
2025-08-19 01:16:25 +0530

    some optimizations using AI tool

--- sorting/Ass.cpp
+++ sorting/Ass.cpp
@@ -8,17 +8,22 @@ void print(int arr[], int n)
     }
 }
 
+// bubblesort in descending order
 void bubbleSort(int arr[], int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if (arr[i] < arr[j])
+            if (arr[j] < arr[j + 1])
             {
-                swap(arr[i], arr[j]);
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
+        if (!swapped)
+            break;
     }
     print(arr, n);
 }
