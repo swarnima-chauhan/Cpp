41b4103e9545e4d4e48d1483e26bb03c3d4c61bd
swarnima-chauhan <swarnimc130101@gmail.com>
2025-08-19 20:29:36 +0530

    Starting 2D array

---
+++ 2Darray/code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int students[3][3] = {{100, 100, 100},
+                          {85, 74, 89},
+                          {63, 72, 65}};
+
+    cout << students[1][1];
+
+    return 0;
+}
