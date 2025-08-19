4aacb53e0e9c7cd2daa72cc0d907cbad5338d038
swarnima-chauhan <swarnimc130101@gmail.com>
2025-08-20 02:54:46 +0530

    done with diagonalSum

--- 2Darray/diagonalSum.cpp
+++ 2Darray/diagonalSum.cpp
@@ -33,7 +33,8 @@ int diagonalSum1(int matrix[][3], int n, int m)
     return psum + ssum;
 }
 
-int diagonalSum(int matrix[][3], int n, int m)
+// shraddha didi
+int diagonalSum2(int matrix[][3], int n, int m)
 {
     int sum = 0;
     for (int i = 0; i < n; i++)
@@ -53,11 +54,25 @@ int diagonalSum(int matrix[][3], int n, int m)
     return sum;
 }
 
+int diagonalSum(int matrix[][3], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += matrix[i][i];
+        if (i != n - i - 1)
+        {
+            sum += matrix[i][n - i - 1];
+        }
+    }
+    return sum;
+}
+
 int main()
 {
     int matrix[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
 
     int matrix1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    cout << diagonalSum1(matrix1, 3, 3);
+    cout << diagonalSum(matrix1, 3);
     return 0;
 }
