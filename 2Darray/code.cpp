c5b6475d9c17c99ef06f39e3b91febcdbeb1ce6d
swarnima-chauhan <swarnimc130101@gmail.com>
2025-08-19 20:32:59 +0530

    traversing 2D array

--- 2Darray/code.cpp
+++ 2Darray/code.cpp
@@ -3,11 +3,23 @@ using namespace std;
 
 int main()
 {
+    // n*m ; n=row, m=column
     int students[3][3] = {{100, 100, 100},
                           {85, 74, 89},
                           {63, 72, 65}};
 
-    cout << students[1][1];
+    // cout << students[1][1];
+
+    int n = 3;
+    int m = 3;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << students[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
